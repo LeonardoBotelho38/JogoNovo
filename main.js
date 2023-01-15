dddee887df9e78406895ff9ebef9d4a1c6b13111
Leonardo Botelho <109173085+LeonardoBotelho38@users.noreply.github.com>
2023-01-14 21:54:15 -0300

    add função selecionaElemento e acertouAPergunta

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-const elementos = [];
+const gradeElementos = [];
 const tabuleiro = document.querySelector('.tabuleiro')
-
 const palavrasEmIngles =   [{"ingles": "the", "portugues": "o/a"},
 {"ingles": "be", "portugues": "ser/estar"},
 {"ingles": "to", "portugues": "para"},
@@ -25,93 +24,43 @@ const palavrasEmIngles =   [{"ingles": "the", "portugues": "o/a"},
 {"ingles": "but", "portugues": "mas"},
 {"ingles": "his", "portugues": "dele"},
 {"ingles": "by", "portugues": "por"}]
-
 const pergunta = document.querySelector('.pergunta')
-
-const todosElementos = document.querySelectorAll('elemento')
-
 const palavrasQueJaForamUsadas = []
-
 const resposta = document.querySelector('#resposta')
-const responder = document.querySelector('#responder')
-
+const btnResponder = document.querySelector('#responder')
 let palavraSorteada
 
-for (let i = 1; i <= 100; i++) {
-    const elemento = document.createElement("div")
-    elemento.classList.add("elemento")
-    elemento.id = "elemento" + i
+criaGradeDeElementos();
 
-    tabuleiro.appendChild(elemento)
-    elementos.push(elemento)
+gradeElementos.forEach(el => el.addEventListener("click", selecionaElemento))
+
+btnResponder.addEventListener('click', acertouAPergunta)
+
+
+function criaGradeDeElementos() {
+    for (let i = 1; i <= 100; i++) {
+        const elemento = document.createElement("div");
+        elemento.classList.add("elemento");
+        elemento.id = "elemento" + i;
+
+        tabuleiro.appendChild(elemento);
+        gradeElementos.push(elemento);
+    }
 }
 
-elementos.forEach(el => el.addEventListener("click", pintaBorda))
-
-
-function pintaBorda(event){
-    resposta.value = ''
-   const elemento = event.target 
-   const borda = event.target.getBoundingClientRect()
-   const x = event.clientX
-   const y = event.clientY
+function selecionaElemento(event){
+    const elementoQuadrado = event.target 
+    elementoQuadrado.classList.add('elementoSelecionado')
+    sorteiaPalavras()
     
-   
+}  
 
-   if (x > borda.right -10 && x < borda.right) {
-    elemento.classList.add("bordaDireita")
-    sorteiaPalavras()
-   }else if (x < borda.left + 10 && x > borda.left) {
-    elemento.classList.add("bordaEsquerda")
-    sorteiaPalavras()
-   } else if (y > borda.top && y < borda.top + 10) {
-    elemento.classList.add("bordaDeCima")
-    sorteiaPalavras()
-   } else if (y > borda.bottom - 10 && y < borda.bottom) {
-    elemento.classList.add("bordaDeBaixo")
-    sorteiaPalavras()
-   } 
-
-   responder.addEventListener("click", function(){
-   
+function acertouAPergunta(){
+        
     if(palavraSorteada.portugues.toLowerCase().includes(resposta.value.toLowerCase())){
-        pergunta.innerHTML ='ACERTOU MISERAVI!'
+        pergunta.innerHTML ='ACERTOU MISERAVI!'}
         
-        if (elemento.classList.contains("bordaDireita")) {
-            elemento.classList.remove("bordaDireita")
-            elemento.classList.add("bordaDireitaVerde")
-        }else if (elemento.classList.contains("bordaEsquerda")) {
-            elemento.classList.remove("bordaEsquerda")
-            elemento.classList.add("bordaEsquerdaVerde")
-        }else if (elemento.classList.contains("bordaDeCima")) {
-            elemento.classList.remove("bordaDeCima")
-            elemento.classList.add("bordaDeCimaVerde")
-        }else if (elemento.classList.contains("bordaDeBaixo")) {
-            elemento.classList.remove("bordaDeBaixo")
-            elemento.classList.add("bordaDeBaixoVerde")
-        }
-    }else {
-        
-        pergunta.innerHTML = ` ERROU! RESPOSTA:${palavraSorteada.portugues}`}
-
-        if (elemento.classList.contains("bordaDireita")) {
-            elemento.classList.remove("bordaDireita")
-            elemento.classList.add("bordaDireitaVermelho")
-        }else if (elemento.classList.contains("bordaEsquerda")) {
-            elemento.classList.remove("bordaEsquerda")
-            elemento.classList.add("bordaEsquerdaVermelho")
-        }else if (elemento.classList.contains("bordaDeCima")) {
-            elemento.classList.remove("bordaDeCima")
-            elemento.classList.add("bordaDeCimaVermelho")
-        }else if (elemento.classList.contains("bordaDeBaixo")) {
-            elemento.classList.remove("bordaDeBaixo")
-            elemento.classList.add("bordaDeBaixoVermelho")
-        }
-    })
-
-
-}
-   
+}   
   
 
 function sorteiaPalavras() {
